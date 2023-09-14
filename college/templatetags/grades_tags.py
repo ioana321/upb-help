e808488afa148cf9af87bd8128c58425e0d0b44d
Ioana <zuna.ioana204@gmail.com>
2023-09-14 14:02:32 +0300

    Added semester average

--- college/templatetags/grades_tags.py
+++ college/templatetags/grades_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name="total_points")
 def total_points(subject):
     from college.models import Subject, Seminar, Lab, Course, Exam
@@ -18,11 +19,13 @@ def total_points(subject):
             points += exam.points_earned
     return points
 
+
 @register.filter(name="has_course")
 def has_course(subject):
     from college.models import Subject, Course
     course = subject.courses.first()
     return course
+
 
 @register.filter(name="has_seminar")
 def has_seminar(subject):
@@ -30,8 +33,26 @@ def has_seminar(subject):
     seminar = subject.seminars.first()
     return seminar
 
+
 @register.filter(name="has_lab")
 def has_lab(subject):
     from college.models import Subject, Lab
     lab = subject.labs.first()
     return lab
+
+
+@register.filter(name="average")
+def average(subjects):
+    from college.models import Subject, Seminar, Lab, Course, Exam
+    avg = 0
+    subj_nr = 0
+    for subj in subjects:
+        subj_nr += 1
+        temp = total_points(subj)
+        if temp % 10 >= 5:
+            avg += int((temp/10) + 1)  # 87/10 = 8.... 8+1 = 9
+        else:
+            avg += int(temp/10)
+
+    return avg/subj_nr
+
